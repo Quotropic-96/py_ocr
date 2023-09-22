767be460cf226545b161a0056329b00123651dde
Gerard Solanes <gerardsolher@gmail.com>
2023-09-22 18:21:33 +0200

    Implements complete API call

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
 import requests
 from google.oauth2 import service_account
+from google.auth.transport import requests as grequests
+import base64
+import os
 
 # Constants
 ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
-CREDENTIALS_PATH = 'path_to_your_service_account_key.json'
+CREDENTIALS_PATH = './nadal-ocr-auth.json'
+OUTPUT_DIR = 'output'
 
 def get_access_token(credentials_path):
     """Get access token from service account credentials."""
@@ -12,6 +16,10 @@ def get_access_token(credentials_path):
         credentials_path,
         scopes=['https://www.googleapis.com/auth/cloud-platform']
     )
+    
+    request = grequests.Request()  # Using the Request class from google.auth.transport.requests
+    credentials.refresh(request)   # Refresh the credentials with the request object
+
     return credentials.token
 
 def annotate_image(image_path, access_token):
@@ -49,18 +57,40 @@ def annotate_image(image_path, access_token):
     response = requests.post(ENDPOINT_URL, headers=headers, data=json.dumps(payload))
     response_data = response.json()
 
+    # Save for parsing
+    save_to_json(response_data, 'response_data.json')
+    
     # Check for errors
-    if 'error' in response_data:
-        raise Exception(response_data['error'])
+    if 'error' in response_data['responses'][0]:
+        raise Exception(response_data['responses'][0]['error']['message'])
 
     # Extract text annotations
-    text_annotations = response_data['responses'][0]['textAnnotations']
+    text_annotations = response_data['responses'][0].get('textAnnotations', [])
+    if not text_annotations:
+        raise Exception("No text annotations found in the image.")
     return text_annotations
+
+def save_to_json(data, filename):
+    # Ensure the output directory exists
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    
+    # Join the path with the output directory
+    filepath = os.path.join(OUTPUT_DIR, filename)
+    
+    with open(filepath, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    filepath = os.path.join(OUTPUT_DIR, filename)
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
 
 # Main execution
 if __name__ == "__main__":
     access_token = get_access_token(CREDENTIALS_PATH)
-    annotations = annotate_image('path_to_your_image.png', access_token)
+    annotations = annotate_image('./images/test.png', access_token)
     
     for annotation in annotations:
         print(annotation['description'])
